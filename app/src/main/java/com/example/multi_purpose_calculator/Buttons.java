353b520eb612bf57cb644d3da71938803d86fcd4
lester <lesterarigo21@gmail.com>
2024-05-12 18:52:15 +0800

    Multipurpose calculator

---
+++ app/src/main/java/com/example/multi_purpose_calculator/Buttons.java
@@ -0,0 +1,14 @@
+package com.example.multi_purpose_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
